1d0c7da1f8c7f7e1fe6f2558a8a875b5266a25ae
André Machado <csantos.machado@gmail.com>
2019-08-30 15:20:00 -0300

    adding new selenium integration.

--- test.py
+++ test.py
@@ -126,13 +126,13 @@ class AssessmentTestCases(unittest.TestCase):
 
         confirm_elem = self.driver.find_element_by_xpath("//button[@type='submit']")
         confirm_elem.click()
-
-        import time; time.sleep(10)
-        self.driver.save_screenshot('screenshot.png')
-
-        session_id_elem = wait.until(
-            EC.presence_of_element_located((By.ID, "sessionId"))
-        )
+        #
+        # import time; time.sleep(10)
+        # self.driver.save_screenshot('screenshot.png')
+        #
+        # session_id_elem = wait.until(
+        #     EC.presence_of_element_located((By.ID, "sessionId"))
+        # )
         self.assertIn("order_success.html", self.driver.current_url)
         self.assertTrue(session_id_elem.text)
 
